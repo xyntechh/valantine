4d9321be214d304ccde33ff0c20a079df07b90ac
xyntechh <xyntechinfo@gmail.com>
2026-02-11 22:35:56 +0530

    fixxx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import CelebrationPage from "./CelebrationPage";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [crushName, setCrushName] = useState("Pragati"); // Change this to your crush's name
+  const [crushName, setCrushName] = useState("Pragati"); 
+  // Change this to your crush's name
 
   const goToPage2 = () => setCurrentPage(2);
   const goToPage3 = () => setCurrentPage(3);
